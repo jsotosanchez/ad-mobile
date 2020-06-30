63999b486690ca53b27f35ce8ded7a15832ad3bd
Jose Soto <jose@xoolix.com>
2020-06-29 22:50:02 -0300

    get especialidades con credentials

--- src/screens/reservarTurno/ReservarTurnoPaso1.js
+++ src/screens/reservarTurno/ReservarTurnoPaso1.js
@@ -2,6 +2,7 @@ import React, { useState, useMemo, useContext } from 'react';
 import { View, Text, TouchableOpacity, Alert } from 'react-native';
 import RNPickerSelect from 'react-native-picker-select';
 import { urlEspecialidades } from '../../config/urls';
+import { useOptions } from '../../hooks/useOptions';
 import { useGet } from '../../hooks/useFetch';
 import { styles, pickerStyle } from '../../../styles';
 import { Context as SessionContext } from '../../contextComponents/SessionContext';
@@ -13,9 +14,10 @@ const placeholderEspecialidad = {
 
 export default function ReservarTurnoPaso1({ navigation }) {
   const [especialidad, setEspecialidad] = useState();
-  const { data: especialidades, status } = useGet(urlEspecialidades());
+  const { data: especialidades, status } = useGet(urlEspecialidades(), null, options);
   const context = useContext(SessionContext);
   const pagoAlDia = context.getPagoAlDia();
+  const options = useOptions(context);
 
   const especialidadesPicker = useMemo(
     () =>
@@ -70,7 +72,7 @@ export default function ReservarTurnoPaso1({ navigation }) {
     <View style={styles.container}>
       <View style={styles.centered}>
         <Text style={styles.label}>
-          Disculpa, parece que tienes una deuda. No puedes pedir una cita hasta que pagues
+          Disculpa, parece que tienes una deuda. No puedes pedir una cita hasta que la pagues
         </Text>
       </View>
     </View>
