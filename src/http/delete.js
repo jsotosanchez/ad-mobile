96bcf2d76ee4baec2db836f26c8a6ab53e108f47
Jose Soto <jose@xoolix.com>
2020-06-29 23:00:34 -0300

    credentials on headers

--- src/http/delete.js
+++ src/http/delete.js
@@ -1,8 +1,13 @@
-export const fetchDelete = async url => {
+import { Base64 } from 'js-base64';
+
+const btoa = Base64.btoa;
+
+export const fetchDelete = async (url, { credientals: { usuario, password } }) => {
   const response = await fetch(url, {
     method: 'delete',
     headers: {
       'Content-Type': 'application/json',
+      Authorization: 'Basic ' + btoa(usuario + ':' + password),
     },
   });
   return response.json();
