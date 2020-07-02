153a668a56331432f8ba686985194dc54497fd07
Jose Soto <jose@xoolix.com>
2020-07-01 21:54:52 -0300

    horarios en 00

--- src/screens/CargarAgenda.js
+++ src/screens/CargarAgenda.js
@@ -13,29 +13,7 @@ import { placeholderEspecialidad, placeholderHora } from '../helpers/pickers';
 import { fetchPost } from '../http/post';
 import BurgerMenu from '../navigation/BurgerMenu';
 
-const horarios = [
-  '08:00',
-  '08:30',
-  '09:00',
-  '09:30',
-  '10:00',
-  '10:30',
-  '11:00',
-  '11:30',
-  '12:00',
-  '12:30',
-  '13:00',
-  '13:30',
-  '14:00',
-  '14:30',
-  '15:00',
-  '15:30',
-  '16:00',
-  '16:30',
-  '17:00',
-  '17:30',
-  '18:30',
-];
+const horarios = ['08:00', '09:00', '10:00', '11:00', '12:00', '13:00', '14:00', '15:00', '16:00', '17:00', '18:30'];
 
 export default function CargarAgenda({ navigation }) {
   const context = useContext(SessionContext);
