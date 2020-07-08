252bb3ec32067416cbcc238cf1c02f5fc99e9e99
Jose Soto <jm.soto.sanchez@gmail.com>
2020-07-08 17:12:40 -0300

    calendars in spanish

--- src/helpers/calendar.js
+++ src/helpers/calendar.js
@@ -1,22 +1,6 @@
 import moment from 'moment';
 import { LocaleConfig } from 'react-native-calendars';
 
-export const months = [
-  'Enero',
-  'Febrero',
-  'Marzo',
-  'Abril',
-  'Mayo',
-  'Junio',
-  'Julio',
-  'Agosto',
-  'Septiembre',
-  'Octubre',
-  'Noviembre',
-  'Diciembre',
-];
-
-export const weekdays = ['Do', 'Lu', 'Ma', 'Mi', 'Ju', 'Vi', 'Sa'];
 export const hoy = new Date();
 
 const getDosMesesAdelante = () => {
@@ -33,27 +17,5 @@ export const getSemanaQueViene = () => {
   const dayINeed = 1; //lunes
   return moment().add(1, 'weeks').isoWeekday(dayINeed).add(-1, 'day');
 };
-
-LocaleConfig.locales['es'] = {
-  monthNames: [
-    'Enero',
-    'Febrero',
-    'Marzo',
-    'Abril',
-    'Mayo',
-    'Junio',
-    'Julio',
-    'Agosto',
-    'Septiembre',
-    'Octubre',
-    'Noviembre',
-    'Diciembre',
-  ],
-  monthNamesShort: ['Ene', 'Feb', 'Mar', 'Abr', 'Mayo', 'Jun', 'Jul', 'Ago', 'Sep', 'Oct', 'Nov', 'Dic'],
-  dayNames: ['Dimanche', 'Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi'],
-  dayNamesShort: ['Do', 'Lu', 'Ma', 'Mi', 'Ju', 'Vi', 'Sa'],
-  today: 'Hoy',
-};
-LocaleConfig.defaultLocale = 'es';
 
 export const DATEFORMAT = 'YYYY-MM-DD';
